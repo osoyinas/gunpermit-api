ec7cdabeffd1da18f9854bbffdd671654e5bdc80
osoyinas <osoyinas@gmail.com>
2024-01-21 19:17:40 +0100

    Add register view

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterUserView
+from .views import RegisterUserView, CurrentUserView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/',RegisterUserView.as_view(), name='auth_register' )
+    path('register/',RegisterUserView.as_view(), name='auth_register' ),
+    path('me/', CurrentUserView.as_view(), name='me'),
+
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import UserSerializer
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class CurrentUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
