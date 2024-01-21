1f9d2d4b4ada247019168b31328f31045abcb0e8
osoyinas <osoyinas@gmail.com>
2024-01-21 18:49:47 +0100

    Add admin permission or authenticated

--- questions_app/urls.py
+++ questions_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ListCreateTopicsView, RetrieveUpdateDestroyTopicsView, ListQuestionsTopicView, ListCreateQuestionsView, DestroyUpdateQuestionView
+from .views import ListCreateTopicsView, RetrieveUpdateDestroyTopicsView, ListQuestionsTopicView, ListCreateQuestionsView, RetrieveDestroyUpdateQuestionView
 
 
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     path('topics/<int:pk>', RetrieveUpdateDestroyTopicsView.as_view(), name='retrieve_topics'),
     path('topics/<int:topic_id>/questions/', ListQuestionsTopicView.as_view(), name='list_questions_topic'),
     path('questions/', ListCreateQuestionsView.as_view(), name='list_create_questions'),
-    path('questions/<int:pk>', DestroyUpdateQuestionView.as_view(), name='destroy_update_question'),
+    path('questions/<int:pk>', RetrieveDestroyUpdateQuestionView.as_view(), name='destroy_update_question'),
     ]
--- questions_app/views.py
+++ questions_app/views.py
@@ -5,34 +5,54 @@ from .serializers import TopicSerializer, QuestionSerializer
 class ListCreateTopicsView(generics.ListCreateAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [permissions.IsAdminUser,]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated,]
+        return super().get_permissions()
 
 class RetrieveUpdateDestroyTopicsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
-    permission_classes = [permissions.IsAdminUser]
 
+    def get_permissions(self):
+        if self.request.method == 'PUT' or self.request.method == 'PATCH' or self.request.method == 'DELETE':
+            self.permission_classes = [permissions.IsAdminUser,]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated,]
+        return super().get_permissions()
 
 
 class ListCreateQuestionsView(generics.ListCreateAPIView):
     serializer_class = QuestionSerializer
     queryset = QuestionModel.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
 
-
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [permissions.IsAdminUser,]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated,]
+        return super().get_permissions()
 
 class ListQuestionsTopicView(generics.ListAPIView):
     serializer_class = QuestionSerializer
     permission_classes = [permissions.IsAuthenticated]
-
     def get_queryset(self, *args, **kwargs):
         # Obtiene el <topic_id> de la URL
-        topic_id = self.kwargs.get('topic_id')    
+        topic_id = self.kwargs.get('topic_id')
         queryset = QuestionModel.objects.filter(topic__id=topic_id)
         return queryset
 
-class DestroyUpdateQuestionView(generics.RetrieveUpdateDestroyAPIView):
+
+class RetrieveDestroyUpdateQuestionView(generics.RetrieveUpdateDestroyAPIView):
     queryset = QuestionModel.objects.all()
     serializer_class = QuestionSerializer
-    permission_classes = [permissions.IsAdminUser]
+
+    def get_permissions(self):
+        if self.request.method == 'PUT' or self.request.method == 'PATCH' or self.request.method == 'DELETE':
+            self.permission_classes = [permissions.IsAdminUser,]
+        else:
+            self.permission_classes = [permissions.IsAuthenticated,]
+        return super().get_permissions()
