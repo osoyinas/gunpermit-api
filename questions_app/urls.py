c703acec351a12481edc07466e39cbabcde20626
osoyinas <osoyinas@gmail.com>
2024-01-21 14:16:27 +0100

    Add permission classes

--- questions_app/urls.py
+++ questions_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ListCreateTopicsView, RetrieveUpdateDestroyAPIView, ListQuestionsTopicView, ListCreateQuestionsView, DestroyUpdateQuestionView
+from .views import ListCreateTopicsView, RetrieveUpdateDestroyTopicsView, ListQuestionsTopicView, ListCreateQuestionsView, DestroyUpdateQuestionView
 
 
 urlpatterns = [
     path('topics/',ListCreateTopicsView.as_view(), name='list_create_topics'),
-    path('topics/<int:pk>', RetrieveUpdateDestroyAPIView.as_view(), name='retrieve_topics'),
+    path('topics/<int:pk>', RetrieveUpdateDestroyTopicsView.as_view(), name='retrieve_topics'),
     path('topics/<int:topic_id>/questions/', ListQuestionsTopicView.as_view(), name='list_questions_topic'),
     path('questions/', ListCreateQuestionsView.as_view(), name='list_create_questions'),
     path('questions/<int:pk>', DestroyUpdateQuestionView.as_view(), name='destroy_update_question'),
--- questions_app/views.py
+++ questions_app/views.py
@@ -5,20 +5,26 @@ from .serializers import TopicSerializer, QuestionSerializer
 class ListCreateTopicsView(generics.ListCreateAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class RetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+class RetrieveUpdateDestroyTopicsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
+    permission_classes = [permissions.IsAdminUser]
+
 
 
 class ListCreateQuestionsView(generics.ListCreateAPIView):
     serializer_class = QuestionSerializer
     queryset = QuestionModel.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class ListQuestionsTopicView(generics.ListAPIView):
     serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self, *args, **kwargs):
         # Obtiene el <topic_id> de la URL
@@ -29,3 +35,4 @@ class ListQuestionsTopicView(generics.ListAPIView):
 class DestroyUpdateQuestionView(generics.RetrieveUpdateDestroyAPIView):
     queryset = QuestionModel.objects.all()
     serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAdminUser]
