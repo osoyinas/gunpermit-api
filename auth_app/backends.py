7f08e9e5f04bc700b45b8fea38c4d1d4fcd6542b
osoyinas <osoyinas@gmail.com>
2024-09-13 22:35:01 +0200

    Fix error handling in authenticate backend

--- auth_app/backends.py
+++ auth_app/backends.py
@@ -4,10 +4,11 @@ from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        User = get_user_model()
         try:
-            user = User.objects.get(email=username)
-        except user.DoesNotExist:
+            print(username)
+            user = get_user_model().objects.get(email=username)
+            print(user)
+        except get_user_model().DoesNotExist:
             return None
         else:
             if user.check_password(password):
