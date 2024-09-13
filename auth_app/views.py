12e5bf476093bbc72581ea71a5b54f2c7e3c65f2
osoyinas <osoyinas@gmail.com>
2024-09-13 22:18:38 +0200

    Rename UserSerializer

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,18 +1,25 @@
 from rest_framework import generics, permissions
-from .serializers import UserSerializer
+from .serializers import RegisterSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import LoginSerializer
 
-class RegisterUserView(generics.CreateAPIView):
+class RegisterUserView(APIView):
     queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            tokens = serializer.get_tokens(user)
+            return Response(tokens, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CurrentUserView(generics.RetrieveAPIView):
-    serializer_class = UserSerializer
+    serializer_class = RegisterSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
