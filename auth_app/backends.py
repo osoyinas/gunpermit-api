50572d745a25f0e8e22ac298350a005799cf8efb
osoyinas <osoyinas@gmail.com>
2024-09-15 12:17:41 +0200

    Remove prints in EmailBackend

--- auth_app/backends.py
+++ auth_app/backends.py
@@ -5,9 +5,7 @@ from django.contrib.auth import get_user_model
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            print(username)
             user = get_user_model().objects.get(email=username)
-            print(user)
         except get_user_model().DoesNotExist:
             return None
         else:
