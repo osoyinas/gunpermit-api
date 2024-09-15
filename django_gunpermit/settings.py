e7629b507c6e56d7f1916207232050be02f8406b
osoyinas <osoyinas@gmail.com>
2024-09-15 18:27:27 +0200

    Update access token lifetime to 5 minutes

--- django_gunpermit/settings.py
+++ django_gunpermit/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
