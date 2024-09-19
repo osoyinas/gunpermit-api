e1a8c4d1d0f1eaeaaa0ceea1cbdb0431f471222e
Pablo Piñas <osoyinas@gmail.com>
2024-09-19 16:44:39 +0200

    Add user and api client mocks

---
+++ auth_app/mocks.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+def createUserMock(
+        username: str = 'test',
+        email: str = 'test@gmail.com',
+        password: str = 'test'):
+    return get_user_model().objects.create_user(
+        username=username,
+        email=email,
+        password=password)
+
+
+def getAuthenticatedClient(user):
+    if not user:
+        user = createUserMock()
+
+    client = APIClient(format='json')
+    token = RefreshToken.for_user(user)
+    client.credentials(
+        HTTP_AUTHORIZATION='Bearer ' + str(token.access_token))
+    return client
+
+
+def getClient():
+    return APIClient()
