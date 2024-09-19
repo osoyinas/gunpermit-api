3b34ad2ce6cbb8f76dfc818152ca9833140651f3
Pablo Piñas <osoyinas@gmail.com>
2024-09-19 16:47:21 +0200

    Add retrieve_quiz test

--- quizzes_app/tests.py
+++ quizzes_app/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+from auth_app.mocks import createUserMock, getAuthenticatedClient
+from .mocks import createQuizMock
 
-# Create your tests here.
+
+class QuizTests(TestCase):
+    def setUp(self):
+        self.user = createUserMock()
+        self.client = getAuthenticatedClient(self.user)
+        self.quiz = createQuizMock()
+
+    def test_retrieve_quiz(self):
+        url = reverse('retrieve_quiz', kwargs={'pk': self.quiz.id})
+
+        response = self.client.get(url)
+
+        print(response.data['questions'])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['title'], self.quiz.title)
+        self.assertEqual(response.data['description'], self.quiz.description)
+        self.assertIsInstance(response.data['questions'], list)
