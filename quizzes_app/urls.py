332fcc6a4e2197d7c9dbcf6b9ef9738a18aa8f0b
osoyinas <osoyinas@gmail.com>
2024-09-20 20:23:09 +0200

    Change retrieve to destroy view

--- quizzes_app/urls.py
+++ quizzes_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import RetrieveQuiz
+from .views import RetrieveDestroyAPIView
 
 
 urlpatterns = [
-    path('<int:pk>/', RetrieveQuiz.as_view(), name='retrieve_quiz'),
+    path('<int:pk>/', RetrieveDestroyAPIView.as_view(), name='retrieve-destroy-quiz'),
 ]
--- quizzes_app/views.py
+++ quizzes_app/views.py
@@ -1,9 +1,11 @@
 from rest_framework import generics, permissions
 
+from auth_app.permissions import IsAdminOrReadOnly
 from quizzes_app.models import QuizModel
 from quizzes_app.serializers import QuizSerializer
 
 
-class RetrieveQuiz(generics.RetrieveAPIView):
+class RetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
     queryset = QuizModel.objects.all()
     serializer_class = QuizSerializer
+    permission_classes = [IsAdminOrReadOnly,]
