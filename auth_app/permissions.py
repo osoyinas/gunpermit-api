6cacf4fc5bb79938a52ae5b1f0bb2a09f9c5e822
osoyinas <osoyinas@gmail.com>
2024-09-21 16:58:26 +0200

    Refactor comments in IsAdminOrReadOnly permission class

--- auth_app/permissions.py
+++ auth_app/permissions.py
@@ -7,8 +7,8 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     """
     
     def has_permission(self, request, view):
-        # Todos los usuarios autenticados pueden ver (GET, HEAD, OPTIONS)
+        # All authenticated users can view (GET, HEAD, OPTIONS)
         if request.method in permissions.SAFE_METHODS:
             return request.user.is_authenticated
-        # Solo los administradores pueden editar (POST, PUT, PATCH, DELETE)
+        # Only administrators can edit (POST, PUT, PATCH, DELETE)
         return request.user.is_staff
