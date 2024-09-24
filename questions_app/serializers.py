e4d4cba92d93c6a063e5d4024367b13783c97d3b
Pablo Piñas <osoyinas@gmail.com>
2024-09-24 11:15:14 +0200

    Format with autopep

--- questions_app/serializers.py
+++ questions_app/serializers.py
@@ -1,24 +1,21 @@
 from rest_framework import serializers
-from .models import TopicModel, QuestionModel 
+from .models import TopicModel, QuestionModel
 
 ANSWERS_STRUCTURE = [{'answer': str, 'is_true': bool}] * 3
 DEFAULT_ANSWERS = [{'answer': "respuesta", 'is_true': False}] * 3
 
+
 class QuestionSerializer(serializers.ModelSerializer):
     answers = serializers.JSONField()
+
     class Meta:
         model = QuestionModel
-        fields = ['id','topic', 'question', 'answers']
+        fields = ['id', 'topic', 'question', 'answers']
 
 
 class TopicSerializer(serializers.ModelSerializer):
     questions = QuestionSerializer(many=True, read_only=True)
+
     class Meta:
         model = TopicModel
         fields = ['id', 'name', 'questions']
-    
-    
-
-
-
-
