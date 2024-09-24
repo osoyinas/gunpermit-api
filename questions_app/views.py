50c831925ee8b3a2ab4ca8a5843516d068fac0c3
Pablo Piñas <osoyinas@gmail.com>
2024-09-24 11:16:49 +0200

    Use isAdminOrReadOnly in questions views

--- questions_app/views.py
+++ questions_app/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics, permissions
+
+from auth_app.permissions import IsAdminOrReadOnly
 from .models import TopicModel, QuestionModel
 from .serializers import TopicSerializer, QuestionSerializer
 
@@ -6,37 +8,19 @@ from .serializers import TopicSerializer, QuestionSerializer
 class ListCreateTopicsView(generics.ListCreateAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
-
-    def get_permissions(self):
-        if self.request.method == 'POST':
-            self.permission_classes = [permissions.IsAdminUser,]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated,]
-        return super().get_permissions()
+    permission_classes = [IsAdminOrReadOnly, ]
 
 
 class RetrieveUpdateDestroyTopicsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = TopicModel.objects.all()
     serializer_class = TopicSerializer
-
-    def get_permissions(self):
-        if self.request.method == 'PUT' or self.request.method == 'PATCH' or self.request.method == 'DELETE':
-            self.permission_classes = [permissions.IsAdminUser,]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated,]
-        return super().get_permissions()
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ListCreateQuestionsView(generics.ListCreateAPIView):
     serializer_class = QuestionSerializer
     queryset = QuestionModel.objects.all()
-
-    def get_permissions(self):
-        if self.request.method == 'POST':
-            self.permission_classes = [permissions.IsAdminUser,]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated,]
-        return super().get_permissions()
+    permission_classes = [IsAdminOrReadOnly, ]
 
 
 class ListQuestionsTopicView(generics.ListAPIView):
@@ -53,10 +37,4 @@ class ListQuestionsTopicView(generics.ListAPIView):
 class RetrieveDestroyUpdateQuestionView(generics.RetrieveUpdateDestroyAPIView):
     queryset = QuestionModel.objects.all()
     serializer_class = QuestionSerializer
-
-    def get_permissions(self):
-        if self.request.method == 'PUT' or self.request.method == 'PATCH' or self.request.method == 'DELETE':
-            self.permission_classes = [permissions.IsAdminUser,]
-        else:
-            self.permission_classes = [permissions.IsAuthenticated,]
-        return super().get_permissions()
+    permission_classes = [IsAdminOrReadOnly, ]
