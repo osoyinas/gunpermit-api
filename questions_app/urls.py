55148ae38ef6a35bd4d2a7d4541e507ab35e3416
Pablo Piñas <osoyinas@gmail.com>
2024-09-24 11:15:47 +0200

    Add / to urls with parameters

--- questions_app/urls.py
+++ questions_app/urls.py
@@ -4,9 +4,13 @@ from .views import ListCreateTopicsView, RetrieveUpdateDestroyTopicsView, ListQu
 
 
 urlpatterns = [
-    path('topics/',ListCreateTopicsView.as_view(), name='list_create_topics'),
-    path('topics/<int:pk>', RetrieveUpdateDestroyTopicsView.as_view(), name='retrieve_topics'),
-    path('topics/<int:topic_id>/questions/', ListQuestionsTopicView.as_view(), name='list_questions_topic'),
-    path('questions/', ListCreateQuestionsView.as_view(), name='list_create_questions'),
-    path('questions/<int:pk>', RetrieveDestroyUpdateQuestionView.as_view(), name='destroy_update_question'),
-    ]
+    path('topics/', ListCreateTopicsView.as_view(), name='list_create_topics'),
+    path('topics/<int:pk>/', RetrieveUpdateDestroyTopicsView.as_view(),
+         name='retrieve_topics'),
+    path('topics/<int:topic_id>/questions/',
+         ListQuestionsTopicView.as_view(), name='list_questions_topic'),
+    path('questions/', ListCreateQuestionsView.as_view(),
+         name='list_create_questions'),
+    path('questions/<int:pk>/', RetrieveDestroyUpdateQuestionView.as_view(),
+         name='retrieve_destroy_update_question'),
+]
