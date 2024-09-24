4823fa191b49d2edcad1c98af23692643f41b3a4
Pablo Piñas <osoyinas@gmail.com>
2024-09-24 11:17:04 +0200

    Format with autopep

--- quizzes_app/urls.py
+++ quizzes_app/urls.py
@@ -5,5 +5,6 @@ from .views import RetrieveDestroyQuizAPIView, CreateQuizAPIView
 
 urlpatterns = [
     path('', CreateQuizAPIView.as_view(), name='create-quiz'),
-    path('<int:pk>/', RetrieveDestroyQuizAPIView.as_view(), name='retrieve-destroy-quiz'),
+    path('<int:pk>/', RetrieveDestroyQuizAPIView.as_view(),
+         name='retrieve-destroy-quiz'),
 ]
