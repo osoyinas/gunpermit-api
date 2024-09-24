cc6573776da1fa3c0ee27b88ca16d9e5096a5148
Pablo Piñas <osoyinas@gmail.com>
2024-09-24 11:15:03 +0200

    Add id to to_string

--- questions_app/models.py
+++ questions_app/models.py
@@ -6,30 +6,36 @@ class TopicModel(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.name + " (ID: " + str(self.id) + ")"
+
 
 ANSWERS_STRUCTURE = [{'answer': str, 'is_true': bool}] * 3
 DEFAULT_ANSWERS = [{'answer': "respuesta", 'is_true': False}] * 3
 
+
 class QuestionModel(models.Model):
     question = models.TextField(max_length=300)
     answers = models.JSONField(default=list)
-    topic = models.ForeignKey(TopicModel, on_delete=models.CASCADE, related_name="questions")
-
+    topic = models.ForeignKey(
+        TopicModel, on_delete=models.CASCADE, related_name="questions")
 
     def clean_answers(self):
         if not isinstance(self.answers, list):
             raise ValidationError("El campo 'answers' debe ser una lista.")
         if len(self.answers) != len(ANSWERS_STRUCTURE):
-            raise ValidationError(f"El campo 'answers' debe tener exactamente {len(ANSWERS_STRUCTURE)} elementos.")
-        
+            raise ValidationError(
+                f"El campo 'answers' debe tener exactamente {len(ANSWERS_STRUCTURE)} elementos.")
+
         for expected_answer, received_answer in zip(ANSWERS_STRUCTURE, self.answers):
             if not isinstance(received_answer, dict):
-                raise ValidationError("Cada elemento en 'answers' debe ser un diccionario.")
-            
+                raise ValidationError(
+                    "Cada elemento en 'answers' debe ser un diccionario.")
+
             for key, expected_type in expected_answer.items():
                 if key not in received_answer:
-                    raise ValidationError(f"Falta la clave '{key}' en uno de los elementos de 'answers'.")
-                
+                    raise ValidationError(
+                        f"Falta la clave '{key}' en uno de los elementos de 'answers'.")
+
                 if not isinstance(received_answer[key], expected_type):
-                    raise ValidationError(f"El valor de '{key}' en 'answers' debe ser de tipo {expected_type}.")
+                    raise ValidationError(
+                        f"El valor de '{key}' en 'answers' debe ser de tipo {expected_type}.")
