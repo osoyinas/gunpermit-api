0702fbdde5e2c753ba8f308859dde915ad53a467
osoyinas <osoyinas@gmail.com>
2024-10-03 16:51:47 +0200

    Rollback to include questions in TopicSerializer

--- questions_app/serializers.py
+++ questions_app/serializers.py
@@ -15,6 +15,8 @@ class QuestionSerializer(serializers.ModelSerializer):
 
 class TopicSerializer(serializers.ModelSerializer):
 
+    questions = QuestionSerializer(many=True, read_only=True)
+
     class Meta:
         model = TopicModel
-        fields = ['id', 'name',]
+        fields = ['id', 'name', 'questions']
