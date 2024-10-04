e9494f0d84a9479cb69e8a20066ad3fddef00970
osoyinas <osoyinas@gmail.com>
2024-10-04 13:06:42 +0200

    Refactor admin.py to include QuizQuestionModel inline in QuizModelAdmin

--- quizzes_app/admin.py
+++ quizzes_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import QuizModel, QuestionModel, QuizQuestionModel
 
-# Register your models here.
+class QuizQuestionInline(admin.TabularInline):
+    model = QuizQuestionModel
+    extra = 1
+
+class QuizModelAdmin(admin.ModelAdmin):
+    inlines = [QuizQuestionInline]
+
+admin.site.register(QuizModel, QuizModelAdmin)
