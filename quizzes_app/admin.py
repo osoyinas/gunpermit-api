2bb7a919615557316146ab1fc0a3043278f22bf7
osoyinas <osoyinas@gmail.com>
2024-10-14 17:01:42 +0200

    Register QuizResultModel in admin.py

--- quizzes_app/admin.py
+++ quizzes_app/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
-from .models import QuizModel, QuestionModel, QuizQuestionModel
+from .models import QuizModel, QuestionModel, QuizQuestionModel, QuizResultModel
+
 
 class QuizQuestionInline(admin.TabularInline):
     model = QuizQuestionModel
     extra = 1
 
+
 class QuizModelAdmin(admin.ModelAdmin):
     inlines = [QuizQuestionInline]
 
+
 admin.site.register(QuizModel, QuizModelAdmin)
+admin.site.register(QuizResultModel)
