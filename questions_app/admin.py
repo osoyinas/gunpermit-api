c4e72b48bded18e4382abf029d3b0e6260ec811f
osoyinas <osoyinas@gmail.com>
2024-10-14 17:01:37 +0200

    Register UserQuestionAttemptModel in admin.py

--- questions_app/admin.py
+++ questions_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TopicModel, QuestionModel
+from .models import TopicModel, QuestionModel, UserQuestionAttemptModel
 # Register your models here.
 admin.site.register(TopicModel)
 admin.site.register(QuestionModel)
+admin.site.register(UserQuestionAttemptModel)
