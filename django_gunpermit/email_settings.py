a0f8deb2559cd8e95d183d5a2740becb0ad0e1c4
osoyinas <osoyinas@gmail.com>
2024-10-15 21:35:24 +0200

    Add email settings

---
+++ django_gunpermit/email_settings.py
@@ -0,0 +1,9 @@
+import os
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
