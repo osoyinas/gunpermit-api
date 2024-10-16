5fae1f55f4f08206d5ce4eb3e4b177f1c05c0504
osoyinas <osoyinas@gmail.com>
2024-10-16 13:36:00 +0200

    Add static files configuration

--- django_gunpermit/production_settings.py
+++ django_gunpermit/production_settings.py
@@ -1,5 +1,6 @@
 
 
+from .email_settings import *
 import os
 from pathlib import Path
 from decouple import config
@@ -10,7 +11,8 @@ from django_gunpermit.settings import BASE_DIR
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [
+                       s.strip() for s in v.split(',')])
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -26,7 +28,32 @@ DATABASES = {
     }
 }
 
-CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=lambda v: [s.strip() for s in v.split(',')])
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS', cast=lambda v: [
+                              s.strip() for s in v.split(',')])
 
-#Load email settings
-from .email_settings import *
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenose.storage.CompressedStaticFilesStorage',
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# Load email settings
