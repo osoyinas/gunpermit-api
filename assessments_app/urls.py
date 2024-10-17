77f2890c8bafe32213177560261b64903d1edcef
osoyinas <osoyinas@gmail.com>
2024-10-17 22:17:17 +0200

    Rename next-assessment endpoint

--- assessments_app/urls.py
+++ assessments_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from assessments_app.views import ListCreateAssessment, ListCreatePlace, RetrieveUpdateDestroyAssessment
+from assessments_app.views import ListCreateAssessment, ListCreatePlace, RetrieveUpdateDestroyAssessment, NextAssessmentByPlace
 
 
 
 urlpatterns = [
     path('places/', ListCreatePlace.as_view(), name='list_create_place'),
+    path('places/<int:pk>/next-assessment/', NextAssessmentByPlace.as_view(), name='next_assessment'),
     path('assessments/', ListCreateAssessment.as_view(), name='list_create_assessment'),
     path('assessments/<int:pk>/', RetrieveUpdateDestroyAssessment.as_view(), name='retrieve_update_destroy_assessment'),
-    path('assessments/next/<int:pk>', RetrieveUpdateDestroyAssessment.as_view(), name='next_assessment'),
 ]
