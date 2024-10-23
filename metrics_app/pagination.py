0113ca6d813da75f5a454537cb9fb8c1f8174f92
osoyinas <osoyinas@gmail.com>
2024-10-23 14:42:04 +0200

    Add MAX_SIZE=100 and DEFAULT_SIZE = 10 in custom metrics pagination

--- metrics_app/pagination.py
+++ metrics_app/pagination.py
@@ -1,13 +1,15 @@
 from rest_framework.pagination import PageNumberPagination
 
+MAX_SIZE = 100
+DEFAULT_SIZE = 10
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    page_size = DEFAULT_SIZE
 
     def get_page_size(self, request):
         size = request.query_params.get('size', None)
         if size is not None and size.isdigit():
             size = int(size)
-            if size < 10:
+            if size < MAX_SIZE:
                 return size
         return self.page_size
