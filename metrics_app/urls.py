1ae13e8f9d571c93a7080ddd56f6428fb593d9cc
osoyinas <osoyinas@gmail.com>
2024-10-23 17:35:30 +0200

    Fix results url with query params

--- metrics_app/urls.py
+++ metrics_app/urls.py
@@ -4,6 +4,6 @@ from metrics_app.views import ListTopicResults, ListUserResults
 
 
 urlpatterns = [
-    path('results/', ListUserResults.as_view(), name='results'),
+    path('results', ListUserResults.as_view(), name='results'),
     path('results/topics/', ListTopicResults.as_view(), name='topic_results'),
 ]
