37dc4de4526aa9685d4d127ad2e3711f9f4eaf91
osoyinas <osoyinas@gmail.com>
2024-10-25 13:55:34 +0200

    use decouple config to get enviroment variables

--- django_gunpermit/email_settings.py
+++ django_gunpermit/email_settings.py
@@ -1,9 +1,13 @@
 import os
+from decouple import config
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+print("EMAIL_HOST_USER: ", EMAIL_HOST_USER)
+print("EMAIL_HOST_PASSWORD: ", EMAIL_HOST_PASSWORD)
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
