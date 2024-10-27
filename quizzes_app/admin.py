b9a373e879d35ef4946fb2cf1f6fc11d8eb6bae7
osoyinas <osoyinas@gmail.com>
2024-10-27 16:24:55 +0100

    Add better admin page in Quizzes with filter and table columns

--- quizzes_app/admin.py
+++ quizzes_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import QuizCategoryModel, QuizModel, QuestionModel, QuizQuestionModel, QuizResultModel
+from .models import QuizCategoryModel, QuizModel, QuizQuestionModel, QuizResultModel
 
 
 class QuizQuestionInline(admin.TabularInline):
@@ -9,6 +9,13 @@ class QuizQuestionInline(admin.TabularInline):
 
 class QuizModelAdmin(admin.ModelAdmin):
     inlines = [QuizQuestionInline]
+    list_filter = ('category',)  # Agrega el filtro por categoría
+    list_display = ('title', 'category', 'number',
+                    'num_questions')  # Agrega columnas
+
+    def num_questions(self, obj):
+        return obj.quizquestionmodel_set.count()
+    num_questions.short_description = 'Number of Questions'
 
 
 admin.site.register(QuizModel, QuizModelAdmin)
