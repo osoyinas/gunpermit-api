03f64a8d5ac2a8ead429cac20e3a3ee9502f5e46
Pablo Piñas <osoyinas@gmail.com>
2024-10-28 15:39:57 +0200

    Add TopicMetricsModel to track metrics for each topic

--- metrics_app/models.py
+++ metrics_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from auth_app.models import CustomUser
+from questions_app.models import TopicModel
 
-# Create your models here.
+
+class TopicMetricsModel(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='metrics')
+    topic = models.ForeignKey(
+        TopicModel, on_delete=models.CASCADE, related_name="metrics")
+    mark = models.IntegerField()
+
+    class Meta:
+        unique_together = ('user', 'topic')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'topic'], name='unique_user_topic_metric')
+        ]
+
+    @property
+    def full_mark(self):
+        return self.topic.questions.count()
+
+    def __str__(self):
+        return f'{self.topic} - {self.mark}/{self.full_mark}'
