7f7b978c9981e7c1336fcdb88ab15870e5ac3655
Pablo Piñas <osoyinas@gmail.com>
2024-10-28 15:39:09 +0200

    Fix loading assessments in setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py load_questions
-python3 manage.py load_assessment
+python3 manage.py load_assessments
 python3 manage.py generate_official_tests 10
 python3 manage.py createsuperuser
