f01a4f91e717c3da24627006a9dee5b2e19fd452
Pablo Piñas <osoyinas@gmail.com>
2024-10-28 15:39:41 +0200

    Register signals in MetricsAppConfig.ready()

--- metrics_app/apps.py
+++ metrics_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MetricsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'metrics_app'
+
+    def ready(self):
+        import metrics_app.signals
