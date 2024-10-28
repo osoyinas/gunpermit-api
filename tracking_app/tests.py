1f9b89a8be610dba74190f7b68650e2d694bbf2c
Pablo Piñas <osoyinas@gmail.com>
2024-10-28 14:24:15 +0200

    Add basic test to check UserQuestionAttempt is created

--- tracking_app/tests.py
+++ tracking_app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
 
-# Create your tests here.
+from auth_app.mocks import createUserMock, getAuthenticatedClient
+from quizzes_app.mocks import createQuizMock
+from tracking_app.models import QuizResultModel, UserQuestionAttemptModel
+
+
+class TrackingTests(TestCase):
+    def setUp(self):
+        self.user = createUserMock()
+        self.client = getAuthenticatedClient(self.user)
+        self.quiz = createQuizMock()
+
+    def test_make_quiz(self):
+        quiz_result = QuizResultModel.objects.create(
+            user=self.user,
+            quiz=self.quiz
+        )
+        for question_with_answers in self.quiz.questions.all():
+            quiz_result.answers.create(
+                question=question_with_answers,
+                answer=question_with_answers.correct_answer_index
+            )
+        quiz_result.save()
+        self.assertIsNotNone(quiz_result)
+
+        # Check QuestionAttemptModel creation
+        for question_with_answers in quiz_result.answers.all():
+            question_attempt = UserQuestionAttemptModel.objects.get(
+                user=self.user,
+                question_with_answer=question_with_answers
+            )
+            self.assertIsNotNone(question_attempt)
+        self.assertEqual(quiz_result.score, 100)
