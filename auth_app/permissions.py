650468a62800f35c8b0502f4c867daa697307540
osoyinas <osoyinas@gmail.com>
2024-10-30 15:17:28 +0100

    Change isAdminOrReadOnly permission to use TokenHasScope too

--- auth_app/permissions.py
+++ auth_app/permissions.py
@@ -1,4 +1,7 @@
 from rest_framework import permissions
+from oauth2_provider.contrib.rest_framework.permissions import (
+    IsAuthenticatedOrTokenHasScope,
+)
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     """
@@ -9,6 +12,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         # All authenticated users can view (GET, HEAD, OPTIONS)
         if request.method in permissions.SAFE_METHODS:
-            return request.user.is_authenticated
+            return IsAuthenticatedOrTokenHasScope().has_permission(request, view)
         # Only administrators can edit (POST, PUT, PATCH, DELETE)
         return request.user.is_staff
