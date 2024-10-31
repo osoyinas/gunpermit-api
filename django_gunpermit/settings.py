ac8738b7dfe91913e35962751401c2468ad60549
osoyinas <osoyinas@gmail.com>
2024-10-31 18:10:55 +0100

    Update ACCESS_TOKEN_LIFETIME to 7 days for improved session management

--- django_gunpermit/settings.py
+++ django_gunpermit/settings.py
@@ -153,8 +153,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
