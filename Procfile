b56fe43e0bae706b62855919101898309f46e7e2
osoyinas <osoyinas@gmail.com>
2024-11-29 23:04:03 +0100

    Add railway configuration from template

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: mkdir static && python manage.py collectstatic --no-input && python manage.py migrate && gunicorn django_gunpermit.wsgi --bind 0.0.0.0:8080
--- django_gunpermit/production_settings.py
+++ django_gunpermit/production_settings.py
@@ -30,11 +30,11 @@ MIDDLEWARE = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': config('PGDATABASE'),
+        'USER': config('PGUSER'),
+        'PASSWORD': config('PGPASSWORD'),
+        'HOST': config('PGHOST'),
+        'PORT': config('PGPORT'),
     }
 }
 
