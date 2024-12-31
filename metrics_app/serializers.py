fb99da93f87ca1370dcdfbf45e91d36497f1d272
Pablo Piñas <osoyinas@gmail.com>
2024-12-31 17:43:49 +0100

    Add quiz and category fields to ResultsSerializer

--- metrics_app/serializers.py
+++ metrics_app/serializers.py
@@ -7,10 +7,11 @@ class ResultsSerializer(serializers.ModelSerializer):
     date = serializers.DateTimeField(source='created_at', read_only=True)
     score = serializers.CharField(source='score_str', read_only=True)
     mark = serializers.IntegerField(source='score', read_only=True)
-
+    quiz = serializers.CharField(source='quiz.title', read_only=True)
+    category = serializers.CharField(source='quiz.category.title', read_only=True)
     class Meta:
         model = QuizResultModel
-        fields = ('id', 'date', 'score', 'mark', 'passed')
+        fields = ('id', 'date', 'score', 'mark', 'passed', 'quiz', 'category')
 
 
 class TopicMetricsSerializer(serializers.ModelSerializer):
